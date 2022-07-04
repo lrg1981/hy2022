5c5bd9065aa87b6f5b339d7a26d9c9fa8d0ef0c7
lrg1981 <94234802+lrg1981@users.noreply.github.com>
2022-07-04 21:41:15 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    console.log("启动执行app")
+    wx.cloud.init({
+      env: "yunlrg2022-0gp8l7nw956dc66f" //云开发环境id
+    })
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
